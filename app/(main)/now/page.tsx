7dbb95368ec9eb0fbf1b5b9c3cce682e2ea82024
Harry Waterman <viceversa1024@gmail.com>
2026-04-11 16:27:32 -0700

    last updated

--- app/(main)/now/page.tsx
+++ app/(main)/now/page.tsx
@@ -5,9 +5,10 @@ export default function Now() {
       <ul className="page-text">
         <li>organizing <a href="https://aisafetyuci.org" target="_blank" rel="noopener noreferrer">aisafetyuci.org</a></li>
         <li>thinking about ways to scale AI safety</li>
-        <li>hosting <a href="/zine3">zine-a-thons</a></li>
+        <li>hosting <a href="/zine3">zine-a-thons</a> with Helena and Matthew</li>
         <li>climbing structures</li>
       </ul>
+      <em className="page-text">Last updated 2026-04-11</em>
     </>
   );
 }
