0834975747daeed1778323a5d7786346165e72b8
Harry Waterman <viceversa1024@gmail.com>
2026-04-25 17:16:15 -0700

    zine3 to partiful

--- app/(main)/zine3/page.tsx
+++ app/(main)/zine3/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function Zine3Redirect() {
   useEffect(() => {
-    window.location.replace("https://forms.gle/EXyBM8F676JQ3tgy9");
+    window.location.replace("https://partiful.com/e/z4yytlL8EmvK1TeBGzre");
   }, []);
 
   return <p>Redirecting...</p>;
