8e89bfbe0c40fe077dcdc3e4dc833618324d7974
Harry Waterman <viceversa1024@gmail.com>
2026-06-09 18:04:46 -0700

    summer now

--- app/(main)/now/page.tsx
+++ app/(main)/now/page.tsx
@@ -3,13 +3,13 @@ export default function Now() {
     <>
       <h1>Now</h1>
       <ul className="page-text">
-        <li>organizing <a href="https://aisafetyuci.org" target="_blank" rel="noopener noreferrer">aisafetyuci.org</a></li>
-        <li>events for <a href="https://luma.com/bluedotevents" target="_blank" rel="noopener noreferrer">BlueDot</a></li>
+        <li><a href="https://generatorresidency.org/" target="_blank" rel="noopener noreferrer">generating</a> in Berkeley</li>
+        <li>advising <a href="https://aisafetyuci.org" target="_blank" rel="noopener noreferrer">aisafetyuci.org</a></li>
         <li>thinking about ways to scale AI safety</li>
         <li>hosting <a href="https://zineathon.com" target="_blank" rel="noopener noreferrer">zine-a-thons</a> with Helena and Matthew</li>
         <li>climbing structures</li>
       </ul>
-      <em className="page-text">Last updated 2026-05-04</em>
+      <em className="page-text">Last updated 2026-06-09</em>
     </>
   );
 }
