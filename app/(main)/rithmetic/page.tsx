9261d95903e70ec32fb3b5a7c7e993a44c0c238b
Harry Waterman <viceversa1024@gmail.com>
2026-06-09 17:44:05 -0700

    using rithmetic for projs

--- app/(main)/rithmetic/page.tsx
+++ app/(main)/rithmetic/page.tsx
@@ -3,7 +3,16 @@ export default function Rithmetic() {
     <>
       <h1>Rithmetic</h1>
       <div className="page-text">
-        <p>Things I&apos;ve researched.</p>
+        <h2>Projects</h2>
+        <div>
+          <p><a href="/fieldmap" target="_blank" rel="noopener noreferrer">AI Safety Field Map</a></p>
+        </div>
+
+        <h2>Fun</h2>
+        <div>
+          <p><a href="https://dangerous-multiplayer.fly.dev/" target="_blank" rel="noopener noreferrer">Dangerous Multiplayer</a></p>
+          <p><a href="/dating" target="_blank" rel="noopener noreferrer">Situationship</a></p>
+        </div>
       </div>
     </>
   );
