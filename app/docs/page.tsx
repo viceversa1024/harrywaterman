a24e1b04845f5b7f3418610318cf531739f2bbb8
Harry Waterman <viceversa1024@gmail.com>
2026-07-07 21:02:47 -0700

    docs redirect

---
+++ app/docs/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+
+const DOCS_URL =
+  "https://docs.google.com/document/d/1MfNyR7vdH2XSJ8jRPxrtHb30nkjwryBwzSVF0oUCA2g/edit?tab=t.0";
+
+export default function Docs() {
+  useEffect(() => {
+    window.location.replace(DOCS_URL);
+  }, []);
+
+  return (
+    <>
+      <meta httpEquiv="refresh" content={`0; url=${DOCS_URL}`} />
+      <p>
+        Redirecting to <a href={DOCS_URL}>doc</a>…
+      </p>
+    </>
+  );
+}
