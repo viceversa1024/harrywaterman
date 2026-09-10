1a7e1741db844211bb154bcce1fb4f66cca1d940
Harry Waterman <viceversa1024@gmail.com>
2026-09-10 13:33:45 -0700

    Update main bio wording about artificial intelligence

--- app/(main)/components/HomeContent.tsx
+++ app/(main)/components/HomeContent.tsx
@@ -181,7 +181,7 @@ export default function HomeContent({ altBios }: HomeContentProps) {
       {universe === null ? (
         <>
           <p>Hello, I&apos;m Harry.</p>
-          <p>I care about the future of humans and machines. Powerful artificial intelligence is possible, and when I&apos;m not working to align it, I enjoy writing, talking, wearing sweaters, believing, and seeing. On earth as it is in heaven.</p>
+          <p>I care about the future of humans and machines. Powerful artificial intelligence is coming, and when I&apos;m not working to make it go well, I enjoy writing, talking, wearing sweaters, believing, and seeing. On earth as it is in heaven.</p>
           <p>I&apos;m currently studying math at the University of California, Irvine.</p>
         </>
       ) : (
